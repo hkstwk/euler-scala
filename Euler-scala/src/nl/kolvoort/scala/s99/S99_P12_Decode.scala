1c00292f33b3c1c559ab10dd1952a8bd960fa908
Harm Kolvoort <hkolvoort+github@gmail.com>
2016-01-14 22:07:08 +0100

    Simplified solution for P12 using List.fill instead of my custom build make function

--- Euler-scala/src/nl/kolvoort/scala/s99/S99_P12_Decode.scala
+++ Euler-scala/src/nl/kolvoort/scala/s99/S99_P12_Decode.scala
@@ -4,18 +4,8 @@ package nl.kolvoort.scala.s99
 object S99_P12_Decode {
   val ls = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
   
-  def decode[A](ls : List[(Int, A)]) : List[Any] = 
-    ls flatMap { x => make(x._1, x._2) }
-  
-  def make[A](t : Int, li: A) : List[A] = {
-    var l = List[A]()
-    var a = 0
-    for (a <- 1 to t) {
-      l = li :: l
-    } 
-    return l
-  }
-  
+  def decode[A](ls : List[(Int, A)]) : List[Any] = ls flatMap { x => List.fill(x._1)(x._2) }
+    
   def main(args: Array[String]): Unit = {
     println(decode(ls))
   }
